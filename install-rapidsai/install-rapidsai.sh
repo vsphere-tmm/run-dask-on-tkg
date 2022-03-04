e45f9d84e9874b24948600934c3c82c7c8a2b211
cwei44 <weichen621@hotmail.com>
2022-03-03 21:26:34 -0800

    Update install-rapidsai.sh

--- install-rapidsai/install-rapidsai.sh
+++ install-rapidsai/install-rapidsai.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+VOLUME_NAME=external-nfs-storage
+CLAIM_NAME=external-nfs-pvc
+MOUNT_PATH="/ml-share"
+PIP_CACHE_CLAIM_NAME=external-nfs-pip-cache-pvc
+PIP_CACHE_MOUNT_PATH="/root/.cache/pip"
+PIP_CACHE_VOLUME_NAME=pip-cache-external-nfs-storage
+RUN_TIME=21.10-cuda11.2-runtime-ubuntu20.04
+REPO="nvcr.io/nvidia/rapidsai/rapidsai"
+SERVICE_TYPE="LoadBalancer"
+WORKER_REPLICAS=7
+WORKER_CPU=6
+WORKER_MEMORY=48G
+SCHEDULER_CPU=3
+SCHEDULER_MEMORY=8G
+JUPYTER_CPU=3
+JUPYTER_MEMORY=8G
+
 kubectl create namespace rapidsai
 wget https://github.com/rapidsai/helm-chart/archive/refs/heads/main.zip
 unzip main.zip
@@ -8,28 +25,6 @@ kubectl apply -f post-rolebindings.yaml
 
 SCHEDULER_NODE_NAME=`kubectl get nodes | grep scheduler | awk '{print $1}'`
 JUPYTER_NODE_NAME=`kubectl get nodes | grep jupyter | awk '{print $1}'`
-
-VOLUME_NAME=external-nfs-storage
-CLAIM_NAME=external-nfs-pvc
-MOUNT_PATH="/ml-share"
-
-PIP_CACHE_CLAIM_NAME=external-nfs-pip-cache-pvc
-PIP_CACHE_MOUNT_PATH="/root/.cache/pip"
-PIP_CACHE_VOLUME_NAME=pip-cache-external-nfs-storage
-
-RUN_TIME=21.10-cuda11.2-runtime-ubuntu20.04
-REPO="nvcr.io/nvidia/rapidsai/rapidsai"
-
-SERVICE_TYPE="LoadBalancer"
-WORKER_REPLICAS=7
-WORKER_CPU=6
-WORKER_MEMORY=48G
-
-SCHEDULER_CPU=3
-SCHEDULER_MEMORY=8G
-
-JUPYTER_CPU=3
-JUPYTER_MEMORY=8G
 
 cd helm-chart-main
 helm dep update rapidsai
